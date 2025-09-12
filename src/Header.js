a54cd2b11b94c61156809f202d33e9d954cc8951
nishant01k <merobook@gmail.com>
2025-09-12 16:10:37 +0545

    01k

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
     { name: "About", href: "#about" },
     { name: "Skills", href: "#skills" },
     { name: "Projects", href: "#projects" },
-    { name: "Certifications", href: "#certifications" },
+    { name: "Certifications", href: "#certificates" },
     { name: "Contact", href: "#contact" },
   ];
 
